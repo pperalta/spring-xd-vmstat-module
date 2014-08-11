e7ca086292d80330257e1b8dd08c38a3ec250935
Patrick Peralta <pperalta@pivotal.io>
2014-08-11 17:13:48 -0400

    gotta run

--- src/main/java/org/springframework/xd/modules/VMStat.java
+++ src/main/java/org/springframework/xd/modules/VMStat.java
@@ -43,66 +43,69 @@ public class VMStat extends MessageProducerSupport {
 
 		@Override
 		public Void call() throws Exception {
-			while (true) {
-				TupleBuilder tupleBuilder = TupleBuilder.tuple();
-				tupleBuilder.put("hello", "world");
-				sendMessage(MessageBuilder.withPayload(tupleBuilder.build()).build());
+//			while (true) {
+//				TupleBuilder tupleBuilder = TupleBuilder.tuple();
+//				tupleBuilder.put("hello", "world");
+//				sendMessage(MessageBuilder.withPayload(tupleBuilder.build()).build());
+//
+//				try {
+//					Thread.sleep(1000);
+//				}
+//				catch (InterruptedException e) {
+//					throw new RuntimeException(e);
+//				}
+//			}
 
-				try {
-					Thread.sleep(1000);
+			ProcessBuilder builder = new ProcessBuilder();
+			builder.redirectErrorStream(true);
+			builder.command(vmstatCommand.split("\\s"));
+
+			Process process = null;
+			try {
+				process = builder.start();
+
+				InputStreamReader in = new InputStreamReader(new BufferedInputStream(process.getInputStream()));
+				BufferedReader reader = new BufferedReader(in);
+				String line;
+
+				while ((line = reader.readLine()) != null) {
+					TupleBuilder tupleBuilder = TupleBuilder.tuple();
+//					StringTokenizer tokenizer = new StringTokenizer(line);
+
+					try {
+						tupleBuilder.put("ls", line);
+//						tupleBuilder.put("waitingProcessCount", Integer.parseInt(tokenizer.nextToken()))
+//								.put("sleepingProcessCount", Integer.parseInt(tokenizer.nextToken()))
+//								.put("virtualMemoryUsage", Long.parseLong(tokenizer.nextToken()))
+//								.put("freeMemory", Long.parseLong(tokenizer.nextToken()))
+//								.put("bufferMemory", Long.parseLong(tokenizer.nextToken()))
+//								.put("cacheMemory", Long.parseLong(tokenizer.nextToken()))
+//								.put("swapIn", Long.parseLong(tokenizer.nextToken()))
+//								.put("swapOut", Long.parseLong(tokenizer.nextToken()))
+//								.put("bytesIn", Long.parseLong(tokenizer.nextToken()))
+//								.put("bytesOut", Long.parseLong(tokenizer.nextToken()))
+//								.put("interruptsPerSecond", Long.parseLong(tokenizer.nextToken()))
+//								.put("contextSwitchesPerSecond", Long.parseLong(tokenizer.nextToken()))
+//								.put("userCpu", Long.parseLong(tokenizer.nextToken()))
+//								.put("kernelCpu", Long.parseLong(tokenizer.nextToken()))
+//								.put("idleCpu", Long.parseLong(tokenizer.nextToken()));
+
+						sendMessage(MessageBuilder.withPayload(tupleBuilder.build()).build());
+					}
+					catch (Exception e) {
+						// most likely cause is parsing a header line; discard and continue
+					}
 				}
-				catch (InterruptedException e) {
-					throw new RuntimeException(e);
+			}
+			catch (IOException e) {
+			}
+			finally {
+				if (process != null) {
+					process.destroy();
 				}
 			}
 
-//		ProcessBuilder builder = new ProcessBuilder();
-//		builder.redirectErrorStream(true);
-//		builder.command(vmstatCommand.split("\\s"));
-//
-//		Process process = null;
-//		try {
-//			process = builder.start();
-//
-//			InputStreamReader in = new InputStreamReader(new BufferedInputStream(process.getInputStream()));
-//			BufferedReader reader = new BufferedReader(in);
-//			String line;
-//
-//			while ((line = reader.readLine()) != null) {
-//				TupleBuilder tupleBuilder = TupleBuilder.tuple();
-//				StringTokenizer tokenizer = new StringTokenizer(line);
-//
-//				try {
-//					tupleBuilder.put("waitingProcessCount", Integer.parseInt(tokenizer.nextToken()))
-//							.put("sleepingProcessCount", Integer.parseInt(tokenizer.nextToken()))
-//							.put("virtualMemoryUsage", Long.parseLong(tokenizer.nextToken()))
-//							.put("freeMemory", Long.parseLong(tokenizer.nextToken()))
-//							.put("bufferMemory", Long.parseLong(tokenizer.nextToken()))
-//							.put("cacheMemory", Long.parseLong(tokenizer.nextToken()))
-//							.put("swapIn", Long.parseLong(tokenizer.nextToken()))
-//							.put("swapOut", Long.parseLong(tokenizer.nextToken()))
-//							.put("bytesIn", Long.parseLong(tokenizer.nextToken()))
-//							.put("bytesOut", Long.parseLong(tokenizer.nextToken()))
-//							.put("interruptsPerSecond", Long.parseLong(tokenizer.nextToken()))
-//							.put("contextSwitchesPerSecond", Long.parseLong(tokenizer.nextToken()))
-//							.put("userCpu", Long.parseLong(tokenizer.nextToken()))
-//							.put("kernelCpu", Long.parseLong(tokenizer.nextToken()))
-//							.put("idleCpu", Long.parseLong(tokenizer.nextToken()));
-//
-//					sendMessage(MessageBuilder.withPayload(tupleBuilder.build()).build());
-//				}
-//				catch (Exception e) {
-//					// most likely cause is parsing a header line; discard and continue
-//				}
-//			}
-//		}
-//		catch (IOException e) {
-//		}
-//		finally {
-//			if (process != null) {
-//				process.destroy();
-//			}
-//		}		}
+			return null;
 		}
 	}
 
